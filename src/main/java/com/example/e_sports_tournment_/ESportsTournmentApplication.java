541dc16dab8434ef62df35b61eb3655029acd52b
BrendaL0pes <brendamedeiroslopes1@gmail.com>
2026-06-19 17:52:12 -0300

    feat: initialize Spring Boot project structure with Maven wrapper

---
+++ src/main/java/com/example/e_sports_tournment_/ESportsTournmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.e_sports_tournment_;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ESportsTournmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ESportsTournmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/e_sports_tournment_/ESportsTournmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.e_sports_tournment_;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ESportsTournmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
